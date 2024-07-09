6fa72241b1ae3edceb137d4849e448f333596d37
dotunakno <doantuananh06042006@gmail.com>
2024-07-09 23:07:06 +0700

    Help?
    ad

--- Project/HousePrice/main.py
+++ Project/HousePrice/main.py
@@ -20,10 +20,13 @@ y.info()
 def CleanData(data):
     data = data[["YearBuilt","OverallQual","OverallCond","1stFlrSF","2ndFlrSF","MSZoning","LotArea"
                  ,"HouseStyle","Condition1" , "Condition2","RoofMatl","Exterior1st","Exterior2nd"]]
+    data["HouseStyle"].str.replace("")
     return data
 X = CleanData(x)
 X.head()
 X.info()
+#baka
+
 # %% Split Training and Test (80/20)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
