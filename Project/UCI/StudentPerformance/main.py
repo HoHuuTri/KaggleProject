2d0c95cfb34539ae17a3dfef03bb2026b835fba0
Hồ Hữu Trí <edricalbert2006@gmail.com>
2024-07-10 22:25:21 +0700

    update data cleaning

--- Project/UCI/StudentPerformance/main.py
+++ Project/UCI/StudentPerformance/main.py
@@ -8,5 +8,35 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.svm import SVR
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from sklearn.metrics import r2_score
 import seaborn as sns
+
+#%% Import Data
+dataset1 = pd.read_csv('student-mat.csv', delimiter = ";")
+dataset2 = pd.read_csv('student-mat.csv', delimiter = ";")
+
+#%% Data Preprocessing
+dataset1.drop(["G1", "G2"], axis = 1)
+dataset2.drop(["G1", "G2"], axis = 1)
+
+#Label Encoder
+le = LabelEncoder()
+columns1 = ["school", "sex", "address", "famsize", "Pstatus", "schoolsup", "famsup", "paid", "activities",
+           "nursery", "higher", "internet", "romantic"]
+for col in columns1:
+    dataset1[col] = le.fit_transform(dataset1[col])
+    dataset2[col] = le.transform(dataset2[col])
+
+#OneHotEncoder
+columns2 = ["Medu", "Fedu", "Mjob", "Fjob", "reason", "guardian"]
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), columns2)], remainder='passthrough')
+dataset1 = np.array(ct.fit_transform(dataset1))
+dataset2 = np.array(ct.fit_transform(dataset2))
+
+print(dataset1)
+
+# %%
